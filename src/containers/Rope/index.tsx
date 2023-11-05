8d289b8522fbe2b3821c82770d1a68b65625bbfc
hyojinLeee-git <splato8899@gmail.com>
2023-11-05 19:28:56 +0900

    feat: add rope asset

--- src/containers/Rope/index.tsx
+++ src/containers/Rope/index.tsx
@@ -47,6 +47,8 @@ export default function Rope() {
     }, []);
     return (
         <div className="rope">
+            <img src="/Rope/glowing.png" id="light-img" />
+
             <canvas ref={canvasRef}></canvas>
         </div>
     );
