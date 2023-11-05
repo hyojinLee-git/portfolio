c64b9b02d57a5f92bc4959520ac84cb50a01e6cd
hyojinLeee-git <splato8899@gmail.com>
2023-11-05 17:51:19 +0900

    feat: typescript 마이그레이션

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom/client";
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router-dom";
+
+import "./style/index.css";
+import App from "./App";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+            <Route path="/" element={<App />} />
+            {/* <Route path="/rainy-day" element={<RainyDay />} /> */}
+            {/* <Route path="/count-down" element={<CountDown />} /> */}
+            {/* <Route path="/fireworks" element={<Fireworks />} /> */}
+        </Route>
+    )
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <RouterProvider router={router} />
+);
