67783b28208bcaa01fb99c4b48f8383b0eb5a83c
hyojinLeee-git <splato8899@gmail.com>
2023-11-05 22:26:53 +0900

    feat: gui 화면 구현

--- src/containers/Gui/index.tsx
+++ src/containers/Gui/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef } from "react";
 
 import "./style.css";
+import { randomNumBetween } from "../../utils/util";
+import Gui from "./Gui";
 
 export default function GuiPage() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -25,6 +27,18 @@ export default function GuiPage() {
         let now, delta;
         let then = Date.now();
 
+        const TOTAL = 20;
+        const particles: Gui[] = [];
+
+        for (let i = 0; i < TOTAL; i++) {
+            const x = randomNumBetween(0, canvas.width);
+            const y = randomNumBetween(0, canvas.height);
+            const radius = randomNumBetween(30, 80);
+            const vy = randomNumBetween(1, 5);
+            const particle = new Gui(x, y, radius, vy);
+            particles.push(particle);
+        }
+
         function animate() {
             window.requestAnimationFrame(animate); // 프레임을 무한으로 생성
 
@@ -34,10 +48,20 @@ export default function GuiPage() {
             if (delta < interval) return;
 
             ctx.clearRect(0, 0, canvasWidth, canvasHeight); // 이전 프레임을 지우고 새 프레임을 만듦
-            ctx.fillStyle = "red";
-            ctx.fillRect(100, 100, 100, 100);
 
             then = now - (delta % interval);
+
+            particles.forEach((particle) => {
+                particle.update();
+                particle.draw(ctx);
+
+                if (particle.y - particle.radius > canvasHeight) {
+                    particle.y = -particle.radius;
+                    particle.x = randomNumBetween(0, canvasWidth);
+                    particle.radius = randomNumBetween(30, 100);
+                    particle.vy = randomNumBetween(1, 5);
+                }
+            });
         }
 
         window.addEventListener("resize", resize);
@@ -50,6 +74,22 @@ export default function GuiPage() {
     }, []);
     return (
         <div className="gui">
+            <svg>
+                <defs>
+                    <filter id="gooey">
+                        <feGaussianBlur
+                            stdDeviation="40"
+                            in="SourceGraphic"
+                            result="blur1"
+                        />
+                        <feColorMatrix
+                            in="blur1"
+                            mode="matrix"
+                            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 100 -23"
+                        />
+                    </filter>
+                </defs>
+            </svg>
             <canvas ref={canvasRef}></canvas>
         </div>
     );
