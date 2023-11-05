a6df94a8af13088e77786cc4578eedd1f9e0de2c
hyojinLeee-git <splato8899@gmail.com>
2023-11-05 19:27:31 +0900

    feat: add canvas initial setting

--- src/containers/Rope/index.tsx
+++ src/containers/Rope/index.tsx
@@ -1,8 +1,50 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import "./style.css";
 
 export default function Rope() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current!;
+        const canvasParent = canvas.parentElement!;
+        const ctx = canvas.getContext("2d")!;
+
+        let canvasWidth: number, canvasHeight: number;
+
+        function resize() {
+            canvasWidth = canvasParent.clientWidth;
+            canvasHeight = canvasParent.clientHeight;
+            canvas.style.width = canvasWidth + "px";
+            canvas.style.height = canvasHeight + "px";
+            canvas.width = canvasWidth;
+            canvas.height = canvasHeight;
+        }
+
+        let interval = 1000 / 60;
+        let now, delta;
+        let then = Date.now();
+
+        function animate() {
+            window.requestAnimationFrame(animate); // 프레임을 무한으로 생성
+
+            now = Date.now();
+            delta = now - then;
+
+            if (delta < interval) return;
+
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight); // 이전 프레임을 지우고 새 프레임을 만듦
+
+            then = now - (delta % interval);
+        }
+
+        window.addEventListener("resize", resize);
+        resize();
+        animate();
+
+        return () => {
+            window.removeEventListener("resize", resize);
+        };
+    }, []);
     return (
         <div className="rope">
             <canvas ref={canvasRef}></canvas>
