265d1aecf2905e469b1dbc9689736bd03235eb6f
hyojinLeee-git <splato8899@gmail.com>
2023-11-12 22:13:38 +0900

    feat: 순서 변경

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,8 +1,8 @@
 export const PROJECT_LIST = [
     { title: "Bounce String", thumbnail: "", link: "/bounce-string" },
+    { title: "Rainy Day", thumbnail: "", link: "/rainy-day" },
     { title: "Count Down", thumbnail: "", link: "/count-down" },
     { title: "Firworks", thumbnail: "", link: "/fireworks" },
     { title: "Gui", thumbnail: "", link: "/gui" },
-    { title: "Rainy Day", thumbnail: "", link: "/rainy-day" },
     { title: "Rope", thumbnail: "", link: "rope" },
 ];
