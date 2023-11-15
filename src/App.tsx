7c4e1a1acd38ee76c25f00195d99d7f9d8e289f9
hyojinLeee-git <splato8899@gmail.com>
2023-11-15 23:12:19 +0900

    feat: section5 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Section1 from "./containers/Sections/Section1";
 import Section2 from "./containers/Sections/Section2";
 import Section3 from "./containers/Sections/Section3";
 import Section4 from "./containers/Sections/Section4";
+import Section5 from "./containers/Sections/Section5";
 import Navigator from "./layouts/Navigator";
 
 import "./style/App.css";
@@ -17,8 +18,8 @@ function App() {
                 <Section2 />
                 <Section3 />
                 <Section4 />
+                <Section5 />
             </main>
-            <footer></footer>
         </div>
     );
 }
---
+++ src/containers/Sections/Section5/index.tsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+export default function Section5() {
+    return (
+        <section className="section5">
+            <h2>Prize</h2>
+            <ul>
+                <li>대전광역시 공공데이터 활용 경진대회 입선 2022.09.01</li>
+                <li>
+                    한국지능정보사회진흥원 공공데이터 해커톤 대상 수상
+                    2021.12.16
+                </li>
+            </ul>
+        </section>
+    );
+}
