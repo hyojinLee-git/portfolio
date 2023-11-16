fcdea745db4cdf7146d916810b7db268b33bf9d5
hyojinLeee-git <splato8899@gmail.com>
2023-11-16 21:47:18 +0900

    feat: 경력사항 수정

--- src/containers/Sections/Section3/index.tsx
+++ src/containers/Sections/Section3/index.tsx
@@ -17,8 +17,8 @@ export default function Section3() {
                     Nextjs+Typescript+Styled component를 이용해 픽앤플랜 보험
                     프로세스 및 내보험상품 웹뷰 파트 개발
                 </li>
-                <li>플래너 프로필 화면 및 질문답변 기능 구현</li>
                 <li>svg를 이용한 해약환급금 그래프 개발, 도넛차트 개발</li>
+                <li>플래너 프로필 화면 및 질문답변 기능 구현</li>
                 <li>백엔드와의 소통능력 계발</li>
             </ul>
             <h3>공공데이터 청년인턴십(2021.09~2021.12)</h3>
@@ -26,10 +26,6 @@ export default function Section3() {
                 <li>공공데이터 제공을 위한 데이터 가공 업무 담당</li>
                 <li>반복적인 업무 개선을 위한 python 스크립트 작성</li>
                 <li>해커톤 행사 참여 및 대상 수상</li>
-            </ul>
-            <h3>한국전자통신연구원 인턴(2021.01~2021.02)</h3>
-            <ul>
-                <li>열화상 카메라 개발 관련 특허 조사</li>
             </ul>
         </section>
     );
